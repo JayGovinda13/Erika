cff6f0f25a94ed2237711fc0e3d11fc8d5ff943f
JayGovinda13 <juliano.amaral@icloud.com>
2026-09-08 16:39:52 -0300

    build: implementa vite-plugin-image-optimizer

--- site-erika-martins/vite.config.js
+++ site-erika-martins/vite.config.js
@@ -1,7 +1,14 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    ViteImageOptimizer({
+      jpg: { quality: 70 },
+      jpeg: { quality: 70 },
+      png: { quality: 70 }
+    }),
+  ],
 })
